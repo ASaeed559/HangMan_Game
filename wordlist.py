8eb9a25c1da5ee1f29ae44f987fe950ac04af275
Ahmad Ershad Saeed <ershadsaeed559@gmail.com>
2026-09-06 00:03:38 +0100

    words transferred to a separate file.

--- wordlist.py
+++ wordlist.py
@@ -1,3 +1,20 @@
 # words for hangman game:
 
-words = ("apple", "banana", "cherry", "mango", "grape", "strawberry")
+words = (
+    "afghanistan", "albania", "algeria", "andorra", "angola", "argentina",
+    "armenia", "australia", "austria", "azerbaijan", "bahamas", "bahrain",
+    "bangladesh", "barbados", "belgium", "belize", "benin", "bhutan",
+    "bolivia", "botswana", "brazil", "bulgaria", "burundi", "cambodia",
+    "cameroon", "canada", "chad", "chile", "china", "colombia", "comoros",
+    "croatia", "cuba", "cyprus", "denmark", "djibouti", "dominica", "ecuador",
+    "egypt", "estonia", "ethiopia", "fiji", "finland", "france", "gabon",
+    "gambia", "georgia", "germany", "ghana", "greece", "grenada", "guatemala",
+    "guinea", "guyana", "haiti", "honduras", "hungary", "iceland", "india",
+    "indonesia", "iran", "iraq", "ireland", "israel", "italy", "jamaica",
+    "japan", "jordan", "kenya", "kiribati", "kuwait", "laos", "latvia",
+    "lebanon", "lesotho", "liberia", "libya", "madagascar", "malawi",
+    "malaysia", "maldives", "mali", "malta", "mexico", "monaco", "mongolia",
+    "morocco", "nepal", "netherlands", "nicaragua", "nigeria", "norway",
+    "oman", "pakistan", "panama", "paraguay", "peru", "philippines",
+    "poland", "portugal"
+)
